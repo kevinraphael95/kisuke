290ab624fc746ceb8b3302a6468a0190f8064a57
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2025-05-29 02:53:59 +0200

    Update unsetreiatsu.py

--- commands/reiatsu/unsetreiatsu.py
+++ commands/reiatsu/unsetreiatsu.py
@@ -2,6 +2,9 @@
 # 📁 REIATSU - SUPPRESSION DU SALON CONFIGURÉ
 # ──────────────────────────────────────────────────────────────
 
+# ──────────────────────────────────────────────────────────────
+# 📦 IMPORTS
+# ──────────────────────────────────────────────────────────────
 import discord
 from discord.ext import commands
 from supabase_client import supabase
