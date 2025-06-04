c5a9c6c4d99ddb2b37638bc390821d3ab9462052
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2025-06-04 15:55:34 +0200

    Update rpg.py

--- commands/fun/rpg.py
+++ commands/fun/rpg.py
@@ -187,5 +187,5 @@ async def setup(bot: commands.Bot):
     cog = RPGBleach(bot)
     for command in cog.get_commands():
         if not hasattr(command, "category"):
-            command.category = "VAACT"
+            command.category = "Fun"
     await bot.add_cog(cog)
