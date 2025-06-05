b8281eb09799db2a13499a08db08e64a859dd24b
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2025-06-05 23:29:44 +0200

    Update and rename sfm.py to sfm.py

--- commands/bleach/sfm.py
+++ commands/bleach/sfm.py
@@ -117,5 +117,5 @@ class SfmCommand(commands.Cog):
 async def setup(bot):
     cog = SfmCommand(bot)
     for command in cog.get_commands():
-        command.category = "Fun"
+        command.category = "Bleach"
     await bot.add_cog(cog)
