c7e5065d3ffeff6ff09b1b82a1830aa107ca7755
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2025-07-13 18:16:39 +0200

    Update reiatsu.py

--- commands/reiatsu/reiatsu.py
+++ commands/reiatsu/reiatsu.py
@@ -61,7 +61,8 @@ class Reiatsu2Command(commands.Cog):
             classe_text = (
                 f"• Classe : **{classe_nom}**\n"
                 f"• Compétence passive : {CLASSES[classe_nom]['Passive']}\n"
-                f"• Compétence active : {CLASSES[classe_nom]['Active']}"
+                f"• Compétence active : {CLASSES[classe_nom]['Active']}\n"
+                f"(non les compétences ne sont pas encore ajoutées, oui la classe est inutile pour l'instant)"
             )
         else:
             classe_text = "Aucune classe sélectionnée.\nUtilise la commande `!classe` pour en choisir une."
