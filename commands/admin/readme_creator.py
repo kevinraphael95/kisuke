bb51027964a9d4b967380a55976090355926a0a7
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2025-09-19 06:52:32 +0200

    Update readme_creator.py

--- commands/admin/readme_creator.py
+++ commands/admin/readme_creator.py
@@ -102,5 +102,4 @@ async def setup(bot: commands.Bot):
     for command in cog.get_commands():
         if not hasattr(command, "category"):
             command.category = "Admin"
-    await bot.add
-_cog(cog)
+    await bot.add_cog(cog)
